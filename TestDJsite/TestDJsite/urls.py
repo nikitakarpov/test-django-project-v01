c662109eeb09d2b9404da53231de11c1c68252c3
Nikita <nikitakarpov2013@gmail.com>
2018-07-11 21:08:07 +0300

    My first test django project. Commit 003

--- TestDJsite/TestDJsite/urls.py
+++ TestDJsite/TestDJsite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, re_path, include
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
-    re_path(r'^firstapp/', include('firstApp.urls'))
+    re_path(r'^firstapp/', include('firstApp.urls')),
     re_path(r'^$', include('mainpage.urls'))
 ]
