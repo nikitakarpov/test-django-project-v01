bc73b7654b02fbef42d4f4c6b6b09a09059ffc8d
Nikita <nikitakarpov2013@gmail.com>
2018-07-12 01:32:38 +0300

    004 commit.start frontend

--- TestDJsite/TestDJsite/settings.py
+++ TestDJsite/TestDJsite/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'firstApp',
-    'mainpage',
+     'mainpage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
