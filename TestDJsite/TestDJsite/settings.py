2ebe85337d3d0ef1ee7c606a8b0500325ad84581
Nikita <nikitakarpov2013@gmail.com>
2018-07-12 23:54:32 +0300

    commit 005. Front-end testing

--- TestDJsite/TestDJsite/settings.py
+++ TestDJsite/TestDJsite/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'firstApp',
      'mainpage',
+    'front_test_1',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
