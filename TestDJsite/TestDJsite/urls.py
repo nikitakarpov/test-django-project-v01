51fcbea484706c883a5a3f3bdb85d469988ce4ee
Nikita Karpov <nkarpov@agl.local>
2018-07-16 12:39:10 +0300

    009 commit. DB work

--- TestDJsite/TestDJsite/urls.py
+++ TestDJsite/TestDJsite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     re_path(r'^firstapp/', include('firstApp.urls')),
     re_path(r'^', include('mainpage.urls')),
     re_path(r'^front-test-1/', include('front_test_1.urls')),
-    #re_path(r'^blog/', include('blog.urls')),
+    #re_path(r'^blog/', include('blog.urls'))
+]
