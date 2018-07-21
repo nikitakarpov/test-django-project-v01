839bc38e95c1d4c9486f88b1812000369a1fd603
Nikita <nikitakarpov2013@gmail.com>
2018-07-21 12:44:54 +0300

    011 cjmmit

--- TestDJsite/firstApp/models.py
+++ TestDJsite/firstApp/models.py
@@ -8,6 +8,9 @@ class Publisher (models.Model):
     country=models.CharField(max_length=50)
     website=models.URLField()
 
+    def __str__(self):
+        return 'Publisher {}; address - {}'.format(self.name, self.address)
+
 
 class Author (models.Model):
     salutation=models.CharField(max_length=10)
@@ -16,9 +19,15 @@ class Author (models.Model):
     email=models.EmailField()
     headshot=models.ImageField(upload_to='img')
 
+    def __str__(self):
+        return 'Author {} {}'.format(self.first_name, self.last_name)
+
 
 class Book (models.Model):
     title=models.CharField(max_length=100)
     authors=models.ManyToManyField(Author)
     publisher=models.ForeignKey(Publisher, on_delete=models.CASCADE)
     publication_date=models.DateTimeField()
+
+    def __str__(self):
+        return 'Book {}'.format(self.title)
